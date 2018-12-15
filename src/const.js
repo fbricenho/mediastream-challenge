f1ed178a94637285e592776e58967c300e0a6b67
franciscob <francisco.briceno@technoly.com>
2018-12-15 11:04:17 +0100

    Setup const variables for Movie DB endpoint, https://mediastream-challenge.herokuapp.com/ created

---
+++ src/const.js
@@ -0,0 +1,17 @@
+/**
+ * Const
+ * @author Francisco Briceno <fbricenho@gmail.com>
+ */
+
+ /**
+  * MovieDB endpoints
+  */
+export const MOVIE_DB_API_URL = 'https://api.themoviedb.org/3/';
+export const MOVIE_DB_API_MOVIE_URL = 'https://api.themoviedb.org/3/movie/';
+export const MOVIE_DB_API_POPULAR_URL = 'https://api.themoviedb.org/3/discover/movie';
+
+/**
+  * MovieDB API keys
+  */
+export const MOVIE_DB_API_KEY = '381464ac3725bf251f71b5b430ed06a7';
+export const MOVIE_DB_TOKEN_V4 = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODE0NjRhYzM3MjViZjI1MWY3MWI1YjQzMGVkMDZhNyIsInN1YiI6IjVjMTRjYWExYzNhMzY4NjZjYzM2N2MzMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.E6vyRR8scNQ80iU6b-5UcFJKWUmM1-myf8zGduVKYhY';
